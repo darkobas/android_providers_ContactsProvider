f17164df0eb87df00013be86f05b66797ff48f56
blong <blong@codeaurora.org>
2015-11-26 11:12:13 +0800

    Revert "Add supports for SIM and Phone account"
    This reverts commit fc091a15bd8c26b6dc4c5020f12867a04e597bfc.
    
    Change-Id: I244cc9542566f303b2ef8929f08a1d0137810cf8

--- src/com/android/providers/contacts/LegacyApiSupport.java
+++ src/com/android/providers/contacts/LegacyApiSupport.java
@@ -1937,7 +1937,8 @@ public class LegacyApiSupport {
             sb.append(" AND " + RawContacts.ACCOUNT_TYPE + "=");
             DatabaseUtils.appendEscapedSQLString(sb, mAccount.type);
         } else {
-            sb.append("1=1");
+            sb.append(RawContacts.ACCOUNT_NAME + " IS NULL" +
+                    " AND " + RawContacts.ACCOUNT_TYPE + " IS NULL");
         }
     }
 
@@ -1954,10 +1955,8 @@ public class LegacyApiSupport {
             sb.append(" AND " + Groups.ACCOUNT_TYPE + "=");
             DatabaseUtils.appendEscapedSQLString(sb, mAccount.type);
         } else {
-            sb.append(Groups.ACCOUNT_NAME + " = '"
-                    + AccountWithDataSet.LOCAL.getAccountName() + "' AND "
-                    + Groups.ACCOUNT_TYPE + " = '"
-                    + AccountWithDataSet.LOCAL.getAccountType() + "'");
+            sb.append(Groups.ACCOUNT_NAME + " IS NULL" +
+                    " AND " + Groups.ACCOUNT_TYPE + " IS NULL");
         }
     }
 
